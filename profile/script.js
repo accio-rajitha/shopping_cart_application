1dbf1914b0ee4e8fd13dfcad09f099d6ff13a66f
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-04-18 23:41:58 +0530

    script

---
+++ profile/script.js
@@ -0,0 +1,7 @@
+let currUser = localStorage.getItem('currUser');
+if(currUser){
+
+}else{
+    //take the user back to login
+   window.location.href="http://127.0.0.1:5500/shopping_cart_application/shop/login.html";
+}
